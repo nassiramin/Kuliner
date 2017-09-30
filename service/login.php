89c5eeb51a898f411df0afc95e0f8670b0e1bce0
nassiramin <nassiramin11@gmail.com>
2017-09-30 17:25:30 +0700

    versi 2.1

--- service/login.php
+++ service/login.php
@@ -1,3 +1,4 @@
+
 <?php
  
   include '../koneksi.php';
@@ -38,3 +39,4 @@
   }
   
   mysql_close();
+?>
--- service/register.php
+++ service/register.php
@@ -1,42 +1,44 @@
 <?php
 
-include 'koneksi.php';
+include '../koneksi.php';
 
 class usr{}
   
+  $id_user = $_POST["id_user"];
+  $nama_user = $_POST["nama_user"];
   $username = $_POST["username"];
   $password = $_POST["password"];
   $confirm_password = $_POST["confirm_password"];
   
   if ((empty($username))) { 
     $response = new usr();
-    $response->success = 0;
-    $response->message = "Kolom username tidak boleh kosong"; 
+    $response->status = false;
+    $response->pesan = "Kolom username tidak boleh kosong"; 
     die(json_encode($response));
   } else if ((empty($password))) { 
     $response = new usr();
-    $response->success = 0;
-    $response->message = "Kolom password tidak boleh kosong"; 
+    $response->status = false;
+    $response->pesan = "Kolom password tidak boleh kosong"; 
     die(json_encode($response));
   } else if ((empty($confirm_password)) || $password != $confirm_password) { 
     $response = new usr();
-    $response->success = 0;
-    $response->message = "Konfirmasi password tidak sama"; 
+    $response->status = false;
+    $response->pesan = "Konfirmasi password tidak sama"; 
     die(json_encode($response));
   } else {
     if (!empty($username) && $password == $confirm_password){
-      $query = mysql_query("INSERT INTO users (id, username, password) VALUES(0,'".$username."','".$password."')");
+      $query = mysql_query("INSERT INTO users (id_user,nama_user, username, password) VALUES('$id_user','$nama_user','$username','$password')");
       
       if ($query){
         $response = new usr();
-        $response->success = 1;
-        $response->message = "Register berhasil, silahkan login.";
+        $response->status = true;
+        $response->pesan = "Register berhasil, silahkan login.";
         die(json_encode($response));
         
       } else { 
         $response = new usr();
-        $response->success = 0;
-        $response->message = "Username sudah ada";
+        $response->status = false;
+        $response->pesan = "Username sudah ada";
         die(json_encode($response));
       }
     } 
