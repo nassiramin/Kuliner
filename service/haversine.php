dfeff002db227ba9745bdac2377df6abbb43369f
Nasir Amin <nassiramin11@gmail.com>
2017-11-04 20:51:55 +0700

    update review

--- service/haversine.php
+++ service/haversine.php
@@ -1,31 +1,51 @@
-<?php 
-	include "koneksi.php";
-	
-	$lat = $_GET['lat'];
-	$lng = $_GET['lng'];	
-	
-	// sesuaikan ip/laptop atau ip emulator bawaan andorid studio 10.0.2.2
-	$url = "http://192.168.181.2/Kuliner/upload_foto/";
-	
+<?php
+	include "../koneksi.php";
+
+	$lat = $_POST['lat'];
+	$lng = $_POST['lng'];
+
 	// perhitungan haversine formula pada sintak SQL
-	$query = mysql_query("SELECT id_tempat, nama, gambar, (6371 * ACOS(SIN(RADIANS(lat)) * SIN(RADIANS($lat)) + COS(RADIANS(lng - $lng)) * COS(RADIANS(lat)) * COS(RADIANS($lat)))) AS jarak FROM tbl_tempat HAVING jarak < 6371 ORDER BY jarak ASC");
-	
-	$json = array();
-	
-	$no = 0;
-	
-	while($row = mysql_fetch_assoc($query)){
-		$json[$no]['id_tempat'] = $row['id_tempat'];
-		$json[$no]['nama'] = $row['nama'];
-		$json[$no]['gambar'] = $url.$row['gambar'];
-		$json[$no]['jarak'] = $row['jarak'];
-		
-		$no++;
-	}
-	
-	echo json_encode($json);
-	echo mysql_error();
-	
-	mysql_close($koneksi);
-	
+	$query = mysql_query("SELECT * ,
+		(6371 * ACOS(SIN(RADIANS(lat)) * SIN(RADIANS($lat)) + COS(RADIANS(lng - $lng)) * COS(RADIANS(lat)) * COS(RADIANS($lat))))
+		AS jarak FROM tempat HAVING jarak < 6371 ORDER BY jarak ASC");
+
+		$obj = new stdClass();
+		$tmpObj = new stdClass();
+		$json = array();
+
+		$no = 0;
+
+		if (mysql_num_rows($query) > 0)
+		{
+			// while($row = mysql_fetch_assoc($query)){
+				// $json[$no]['id_tempat'] = $row['id_tempat'];
+				// $json[$no]['nama_tempat'] = $row['nama_tempat'];
+				// $json[$no]['url'] = $row['url'];
+				// $json[$no]['jarak'] = $row['jarak'];
+			//
+			// 	$no++;
+			// }
+
+			while($row = mysql_fetch_object($query)){
+				$id= $row->id_tempat;
+				$queryJumlahReview = mysql_query("SELECT * FROM review where id_tempat='$id'");
+				$jumReview= mysql_num_rows($queryJumlahReview);
+				$row->jumlah_review=$jumReview;
+				array_push($json, $row);
+			}
+
+			$tmpObj->list = $json;
+
+			$obj->status= true;
+			$obj->pesan= "sukses";
+			$obj->data= $tmpObj;
+		}else{
+			$obj->status= false;
+			$obj->pesan= "load data gagal";
+			$obj->data= null;
+		}
+		echo json_encode($obj);
+		echo mysql_error();
+
+		mysql_close($koneksi);
 ?>
--- service/review_insert.php
+++ service/review_insert.php
@@ -1,37 +1,47 @@
-
 <?php
 error_reporting(0);
 include "../koneksi.php";
- 	
+
 	$id_user= $_POST['id_user'];
 	$id_tempat	= $_POST['id_tempat'];
 	$isi	= $_POST['isi'];
 	$suka	= $_POST['suka'];
 	$rating_tempat	= $_POST['rating_tempat'];
-	
+
 	$query = mysql_query("INSERT INTO review (id_user,id_tempat,isi,suka,rating_tempat) VALUES('$id_user',
 		'$id_tempat','$isi','$suka','$rating_tempat')");
 	// $sql = mysql_query($query);
 	$obj= new stdClass();
 	if ($query)
 	{
-		
 		$Q= mysql_query("SELECT rating FROM tempat WHERE id_tempat='$id_tempat'") or die(mysql_error());
-		while($row = mysql_fetch_assoc($Q)) 
-		{ 
-    		$rating=($row["rating"]+$rating_tempat)/2; 
-    		$v= mysql_query("UPDATE tempat SET rating='$rating' WHERE id_tempat='$id_tempat' "); 
-    		if ($v){
-    				$obj->status=true;
-					$obj->pesan="sukses";
-    		}
-    		else{
-				$obj->status=false;
-				$obj->pesan="load data gagal";
-			}
-		} 
+    $data = mysql_fetch_array($Q);
 
-		
+    if($data["rating"] == "0"){
+      $v= mysql_query("UPDATE tempat SET rating='$rating_tempat' WHERE id_tempat='$id_tempat' ");
+      if ($v){
+        $obj->status=true;
+        $obj->pesan="sukses";
+      }
+      else{
+        $obj->status=false;
+        $obj->pesan="load data gagal";
+      }
+    }else{
+      while($row = mysql_fetch_assoc($Q))
+      {
+          $rating=($row["rating"]+$rating_tempat) / 2;
+          $v= mysql_query("UPDATE tempat SET rating='$rating' WHERE id_tempat='$id_tempat' ");
+          if ($v){
+              $obj->status=true;
+            $obj->pesan="sukses";
+          }
+          else{
+          $obj->status=false;
+          $obj->pesan="load data gagal";
+        }
+      }
+    }
 	}
 	else{
 		$obj->status=false;
@@ -39,8 +49,8 @@ include "../koneksi.php";
 	}
 
 	echo json_encode($obj);
-	
-	
+
+
 	mysql_close($koneksi);
 
 ?>
